729991f84d8a9223b0050949416fbc922fd43577
Amin Etesamian <aminetesamian1371@gmail.com>
2017-08-12 18:03:25 -0700

    storage and its test

---
+++ tests/test_storage.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from os import path
+
+from tg import expose, TGController, AppConfig
+from webtest import TestApp, Upload
+
+import tgext
+from tgext.utils.storage import store
+
+
+TESTS_DIR = path.abspath(path.join(path.dirname(tgext.__file__), '..', 'tests'))
+PATH_TO_FILE = path.join(TESTS_DIR, 'stuff', 'tg.png')
+PATH_TO_STORAGE = path.join(TESTS_DIR, 'storage')
+
+
+class RootController(TGController):
+    @expose()
+    def post_form(self, **kwargs):
+        stored_filename = store(kwargs.get('file'))
+        return stored_filename
+
+    @expose()
+    def form(self):
+        return '''
+        <form method="POST" action="/post_form">
+            <input type="file" name="file">
+        </form>'''
+
+
+class TestStorageManager(object):
+    @classmethod
+    def setup_class(cls):
+        config = AppConfig(minimal=True, root_controller=RootController())
+        config.paths['static_files'] = path.join(TESTS_DIR, 'stuff')
+        cls.wsgi_app = config.make_wsgi_app()
+
+    def make_app(self, **options):
+        return TestApp(self.wsgi_app)
+
+    def test_file_is_stored(self):
+        app = self.make_app()
+        form = app.get('/form').forms[0]
+
+        form['file'] = Upload(PATH_TO_FILE)
+
+        stored_filename = form.submit().text
+        stored_file_path = path.join(PATH_TO_STORAGE, stored_filename)
+
+        file_checksum = self.md5(PATH_TO_FILE)
+        stored_file_checksum = self.md5(stored_file_path)
+
+        assert file_checksum == stored_file_checksum
+
+    @classmethod
+    def teardown_class(cls):
+        import shutil
+        shutil.rmtree(PATH_TO_STORAGE)
+
+    def md5(self, fname):
+        import hashlib
+        hash_md5 = hashlib.md5()
+        with open(fname, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_md5.update(chunk)
+        return hash_md5.hexdigest()
---
+++ tgext/utils/storage.py
@@ -0,0 +1,33 @@
+import random
+import string
+from os import path, mkdir
+from cgi import FieldStorage
+
+from tg import config
+
+
+def store(data: FieldStorage):
+    extension = tell_extension(data)
+    filename = generate_id(extension)
+    destination = _specify_path(filename)
+
+    with open(destination, mode='wb') as dest:
+        content = data.file.read()
+        dest.write(content)
+        return filename
+
+
+def _specify_path(filename):
+    storage_path = path.join(config['paths']['static_files'], '..', 'storage')
+    if not path.exists(storage_path):
+        mkdir(storage_path)
+    return '{}/{}'.format(storage_path, filename)
+
+
+def tell_extension(file: FieldStorage):
+    return file.filename.split('.')[-1]
+
+
+def generate_id(ext, size=16, chars=string.ascii_lowercase + string.digits):
+    uid = ''.join(random.choice(chars) for _ in range(size))
+    return '{}.{}'.format(uid, ext)
